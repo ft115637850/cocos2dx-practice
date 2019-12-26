5d23ee920cdc3e0660937a5ec63ed37d453a47ef
Zou, Newton Xing <115637850@qq.com>
2019-12-26 22:58:29 +0800

    update

---
+++ MemoryCard/Classes/CardData.h
@@ -0,0 +1,7 @@
+#pragma once
+struct CardData {
+	unsigned int number;
+	unsigned int row;
+	unsigned int column;
+	unsigned int flipCount;
+};
---
+++ MemoryCard/Classes/CardState.h
@@ -0,0 +1,6 @@
+#pragma once
+enum CardState
+{
+	FRONT,
+	BACK
+};
